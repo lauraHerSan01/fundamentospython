9cd81957fd4ec231be1e772c4599af60b885de92
laura <hernandez.sandoval.laura@gmail.com>
2023-06-09 13:24:21 -0600

    02 Variables

---
+++ main.py
@@ -0,0 +1,11 @@
+# funcion print
+print ("Hola DSM 3B")
+
+#Podemos enviar distintos tipos de datos 
+print ("Un string")
+print ("Un entero", 25)
+print ("un float", 3.1416)
+print ("un boolean", True)
+
+
+
---
+++ variables.py
@@ -0,0 +1,14 @@
+# Python es no Tipado 
+
+name = "Laura Hernandez"
+age = 19
+height = 1.75
+is_student = True
+children = []
+
+print(type (name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
+
