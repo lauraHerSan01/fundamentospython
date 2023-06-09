c7f24858ea71ebf9bb1fd9e849ae3ce25b92f09f
laura <hernandez.sandoval.laura@gmail.com>
2023-06-09 13:59:51 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#Operadores Matematicos
+# + - * %
+#/ division float
+#// division entera 
+# ** Potencia
+
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 % 2)
+print(10 / 3)
+print(10 // 3)
+print(3 ** 3)
+
+
+#Operadores para Strings
+# + (concatena) * (replica)
+print("Hola " + "Mundo!")
+print ("Hola \n" * 4)
