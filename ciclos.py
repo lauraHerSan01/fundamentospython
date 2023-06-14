8792843b9aa2aa00db7d447d6f8d2ec5737c210b
laura <hernandez.sandoval.laura@gmail.com>
2023-06-14 11:56:58 -0600

    11 Ciclos

---
+++ ciclos.py
@@ -0,0 +1,31 @@
+#while
+#whiles exp_booleana
+num = 1
+ while num <= 10:
+    print(num)
+    num += 1
+
+#for -> iterable
+my_str = "Hola"
+for letter in my_str:
+    print(letter)
+
+my_lista = ["a", "b", "c", 12]
+print item in my_lista:
+    print(item)
+
+#funcion range()
+#range(fin)
+for i in range(3):
+    print(i, end='')
+    print()
+# range ini(ini:fin:step)
+
+for i in range(3, 6):
+    print(i, end='')
+    print()
+# range ini(ini:fin)
+
+for i in range(1, 11, 2):
+  print(i, end='')
+    print()
